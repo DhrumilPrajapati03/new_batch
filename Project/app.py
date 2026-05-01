31b8fe009e55d2374390786e02758d04f0f7742a
Dhrumil Prajapati <dhrumil.prajapati@clicode.co>
2026-05-01 18:30:02 +0530

    refactor task management operations for clarity and functionality

--- Project/app.py
+++ Project/app.py
@@ -15,6 +15,31 @@ def task():
             add = input("Enter the task you want to add =") #hangout
             tasks.append(add)
             print(f"Task {add} has been successfully added...")
-        break
+
+        elif operation == 2:
+            update_val = input("Enter the taks name you want to update = ")
+            if update_val in tasks:
+                up = input("Enter new task = ")
+                ind = tasks.index(update_val)#2
+                tasks[ind] = up
+                print(f"Updated Task {up}")
+
+        elif operation == 3:
+            del_val = input("Enter the task name you want to delete = ")
+            if del_val in tasks:
+                ind = tasks.index(del_val)
+                del tasks[ind]
+                print(f"Task {del_val} has been successfully deleted")
+
+        elif operation == 4:
+            print(f"Total tasks = {tasks}")
+
+        elif operation == 5:
+            print("Closing the process...")
+            break
+        
+        else:
+            print("Invalid Input")
 
 task()
+# task()
